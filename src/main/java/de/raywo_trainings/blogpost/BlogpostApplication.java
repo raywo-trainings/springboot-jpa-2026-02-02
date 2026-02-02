1c52ed8d149a2a4b83ff4ce8f50614f26ed6e602
Ray Wojciechowski <ray.wojciechowski@ars.de>
2026-02-02 11:22:27 +0100

    Initial commit

---
+++ src/main/java/de/raywo_trainings/blogpost/BlogpostApplication.java
@@ -0,0 +1,13 @@
+package de.raywo_trainings.blogpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostApplication.class, args);
+	}
+
+}
