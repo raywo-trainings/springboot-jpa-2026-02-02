55ca8d38435e56303298f8e45a4260434d4723f0
Ray Wojciechowski <ray.wojciechowski@ars.de>
2026-02-03 10:34:51 +0100

    Add endpoint for single blogpost

--- src/main/java/de/raywo_trainings/blogpost/BlogpostController.java
+++ src/main/java/de/raywo_trainings/blogpost/BlogpostController.java
@@ -1,15 +1,18 @@
 package de.raywo_trainings.blogpost;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class BlogpostController {
 
-  private final List<BlogpostDto> blogposts = new ArrayList<>();
+  private final Map<String, BlogpostDto> blogposts = new HashMap<>();
 
 
   BlogpostController() {
@@ -18,14 +21,35 @@ public class BlogpostController {
 
 
   public void initializeData() {
-    blogposts.add(new BlogpostDto("Blogpost 1", "Text 1", "Author 1", "2021-02-01", "2021-02-01"));
-    blogposts.add(new BlogpostDto("Blogpost 2", "Text 2", "Author 2", "2021-02-02", "2021-02-02"));
+    final BlogpostDto blogpost1 = new BlogpostDto(
+        UUID.randomUUID().toString(),
+        "Blogpost 1",
+        "Text 1",
+        "Author 1",
+        "2021-02-01",
+        "2021-02-01");
+    blogposts.put(blogpost1.getId(), blogpost1);
+
+    final BlogpostDto blogpost2 = new BlogpostDto(
+        UUID.randomUUID().toString(),
+        "Blogpost 2",
+        "Text 2",
+        "Author 2",
+        "2021-02-02",
+        "2021-02-02");
+    blogposts.put(blogpost2.getId(), blogpost2);
   }
 
 
   @GetMapping("/blogposts")
-  public List<BlogpostDto> getBlogposts() {
-    return blogposts;
+  public Collection<BlogpostDto> getBlogposts() {
+    return blogposts.values();
+  }
+
+
+  @GetMapping("/blogposts/{id}")
+  public BlogpostDto getBlogpost(@PathVariable String id) {
+    return blogposts.get(id);
   }
 
 }
--- src/main/java/de/raywo_trainings/blogpost/BlogpostDto.java
+++ src/main/java/de/raywo_trainings/blogpost/BlogpostDto.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class BlogpostDto {
 
+  private String id;
   private String title;
   private String text;
   private String author;
