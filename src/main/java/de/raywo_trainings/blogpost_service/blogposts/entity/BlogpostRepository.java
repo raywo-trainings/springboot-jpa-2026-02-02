1654b2be881058963cdcf7382df75690e553dace
Ray Wojciechowski <ray.wojciechowski@ars.de>
2026-02-06 11:20:52 +0100

    Add support for query params and filtering of blogposts
    # Conflicts:
    #	src/main/java/de/raywo_trainings/blogpost_service/blogposts/entity/BlogpostRepository.java

--- src/main/java/de/raywo_trainings/blogpost_service/blogposts/entity/BlogpostRepository.java
+++ src/main/java/de/raywo_trainings/blogpost_service/blogposts/entity/BlogpostRepository.java
@@ -1,24 +1,13 @@
 package de.raywo_trainings.blogpost_service.blogposts.entity;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.time.ZonedDateTime;
-import java.util.List;
-
 @Repository
-public interface BlogpostRepository extends JpaRepository<BlogpostEntity, String> {
-
-  List<BlogpostEntity> findByAuthor(String author);
-
-  List<BlogpostEntity> findByCreatedAtBetween(
-      ZonedDateTime start,
-      ZonedDateTime end
-  );
-
-  List<BlogpostEntity> findByAuthorAndCreatedAtBetween(
-      String author,
-      ZonedDateTime start,
-      ZonedDateTime end);
+public interface BlogpostRepository
+    extends JpaRepository<BlogpostEntity, @NonNull String>,
+            JpaSpecificationExecutor<BlogpostEntity> {
 
 }
